42861818ccc77ef36843e5ac96ad6fbf961b8daa
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-01-28 17:07:30 -0500

    create users route draft

---
+++ routes/users/users.js
@@ -0,0 +1,31 @@
+app.get("/users", (req, res) => {
+    try {
+
+    } catch(error) {
+
+    };
+});
+
+app.get("/users/:id", (req, res) => {
+    try {
+
+    } catch(error) {
+
+    };
+});
+
+app.post("/users", (req, res) => {
+    try {
+
+    } catch(error) {
+
+    };
+})
+
+app.delete("/users/:id", (req, res) => {
+    try {
+
+    } catch(error) {
+
+    };
+});
