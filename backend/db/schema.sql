392c44959134880b7745f7c165f355387cf0df70
david maravillas <davidmaravillas@pursuit.org>
2020-01-28 20:45:05 -0500

    add time stamp to db elements

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -16,44 +16,51 @@ CREATE TABLE users (
     last_name TEXT NOT NULL,
     age INTEGER,
     profile_image INTEGER FOREIGN KEY REFERENCES pictures(id),
-    about_statement TEXT
+    about_statement TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     album_name TEXT,
-    album_owner INTEGER FOREIGN KEY REFERENCES users(id)
+    album_owner INTEGER FOREIGN KEY REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE pictures (
     id SERIAL PRIMARY KEY,
     picture IMAGE,
     album INTEGER FOREIGN KEY REFERENCES albums(id),
-    poster_id INTEGER FOREIGN KEY REFERENCES users(id)
+    poster_id INTEGER FOREIGN KEY REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     poster_id INTEGER FOREIGN KEY REFERENCES users(id),
-    body TEXT
+    body TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE img_likes (
     id SERIAL PRIMARY KEY,
     img_id INTEGER FOREIGN KEY REFERENCES pictures(id),
-    liker_id INTEGER FOREIGN KEY REFERENCES users(id)
+    liker_id INTEGER FOREIGN KEY REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE posts_likes (
     id SERIAL PRIMARY KEY,
     author_id INTEGER FOREIGN KEY REFERENCES posts(id),
-    liker_id INTEGER FOREIGN KEY REFERENCES users(id)
+    liker_id INTEGER FOREIGN KEY REFERENCES users(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
     author_id INTEGER FOREIGN KEY REFERENCES users(id),
     body TEXT,
-    post_id INTEGER FOREIGN KEY REFERENCES posts(id)
+    post_id INTEGER FOREIGN KEY REFERENCES posts(id),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
