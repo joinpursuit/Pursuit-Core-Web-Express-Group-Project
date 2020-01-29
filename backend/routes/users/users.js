c5e6464fe73151cf67889b2373103a3c831d2333
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-01-28 21:16:16 -0500

    complete draft of users.js routes

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -1,31 +1,62 @@
-app.get("/users", (req, res) => {
+app.get("/", (req, res) => {
     try {
-
+        let users = db.any("SELECT * FROM users");
+        res.json({
+            status: "success",
+            message: "got all users",
+            body: {
+                users
+            }
+        });
     } catch(error) {
-
+        next(err);
     };
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/:id", (req, res) => {
     try {
-
+        let users = db.one ("SELECT * FROM users WHERE id = $1 ", [req.params.id]);
+        res.json({
+            status: "success",
+            message: "got specific user information",
+            body: {
+                users
+            }
+        });
     } catch(error) {
-
+        next(err);
     };
 });
 
-app.post("/users", (req, res) => {
+app.post("/", (req, res) => {
     try {
-
+        let users = await db.one("INSERT INTO users (first_name, last_name, age, profile_image, about_statement) VALUES (${first_name}, ${last_name), ${age}, ${profile_image}, ${about_statement}) RETURNING *", req.body);
+        res.json({
+            status: "success",
+            message: "created new user",
+            body: {
+                name: req.body.name,
+                age: req.body.age
+            }
+        });
     } catch(error) {
-
+        next(err);
     };
 })
 
-app.delete("/users/:id", (req, res) => {
+app.delete("/:id", (req, res) => {
     try {
-
+        await db.none("DELETE FROM users WHERE id = $1", [req.params.id]);
+        res.json({
+            status: "success",
+            message: "deleted a single user",
+            body: {
+                id: req.params.id,
+                name: req.body.name,
+                age: req.body.age
+            }
+        });
     } catch(error) {
-
+        next(err);
     };
 });
