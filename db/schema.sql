c2b9414f21d89d0f4c4be4e84935db7dd009cc89
rafidhossain1995 <rafidhossain@pursuit.org>
2020-01-29 19:54:49 -0500

    insert data into tables

--- db/schema.sql
+++ db/schema.sql
@@ -2,16 +2,17 @@ DROP DATABASE IF EXISTS cipher_db;
 CREATE DATABASE cipher_db;
 
 \c cipher_db;
-
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS likes;
-DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS albums;
 
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    name text,
+    full_name text,
     email text,
     date_of_birth text,
     gender text,
@@ -21,7 +22,7 @@ CREATE TABLE users(
 
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY,
-    users_id INT REFERENCES users(id),
+    posters_id INT REFERENCES users(id),
     body text,
     time_stamp date
 );
@@ -30,30 +31,34 @@ CREATE TABLE likes(
     id SERIAL PRIMARY KEY,
     post_id INT REFERENCES posts(id),
     comment_id INT REFERENCES comments(id),
-    user_id INT REFERENCES users(id)
+    likers_id INT REFERENCES users(id)
 );
 
 
 CREATE TABLE comments(
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    posts_id INT REFERENCES posts(id),
+    commenters_id INT REFERENCES users(id),
+    comment_post_id INT REFERENCES posts(id),
     body text
 );
+
+
 CREATE TABLE albums(
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
+    albums_id INT REFERENCES users(id),
     name text, 
     thumbnail text, 
     time_stamp date
-
 );
+
+
 CREATE TABLE photos(
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    albums_id INT REFERENCES albums(id),
+    table_id INT REFERENCES users(id),
+    photos_id INT REFERENCES albums(id),
     photo_url text,
     time_stamp date
-
-
 );
+
+-- INSERT INTO users (id, name, email, date_of_birth, gender, profile_pic, join_date)
+-- VALUES(1, "Rafid", "rafidhos9@gmail.com", "July 11 1995", "Male", "Profile Pic added", "07/11/2015");
