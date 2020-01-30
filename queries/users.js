64c673546b4e554385ab87fb43863142b9a28d41
rafidhossain1995 <rafidhossain@pursuit.org>
2020-01-29 21:54:42 -0500

    create likes and users queries

---
+++ queries/users.js
@@ -0,0 +1,66 @@
+// Get all users
+
+
+const getAllUsers = async(res, req, next) =>{
+    try{
+        let users = await db.any("SELECT * FROM users")
+        res.status(200).json({
+            status:"Success",
+            message: "Got All Users",
+            body: users
+        })
+
+    }catch(err){
+        next(err)
+    }
+}
+//getAllUsers()
+
+// Get single users
+
+const getSingleUsers = async(res, req, next) =>{
+    try{
+        let user = await db.one("SELECT * FROM USERS WHERE ID = $1", req.params.id)
+        res.status(200).json({
+            status:"success",
+            message: "Got Single User",
+            body: user
+        })
+
+    } catch(err){
+        next(err)
+    }
+}
+
+//Add single user
+
+const addSingleUser = async(res, req, next)=>{
+    try{
+        let newUser = await db.one("INSERT INTO USERS(full_name, email, date_of_birth, gender, profile_pic, join_date) VALUES (${full_name}, ${email}, ${date_of_birth}, ${gender}, ${profile_pic}, ${join_date})", req.body)
+        res.status(200).json({
+            status:"success",
+            message: "Added User",
+            body: newUser
+        })
+
+    }catch(err){
+        next(err)
+    }
+}
+
+// Delete Single User
+
+const deleteSingleUser = async(res, req, next)=>{
+    try{
+        let deleteUser = await db.none("DELETE * FROM USERS WHERE id = $1", req.params.id)
+        res.status(200).json({
+            status:"Success",
+            message:"User deleted",
+            body: deleteSingleUser
+        })
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {getAllUsers, getSingleUsers, addSingleUser, deleteSingleUser};
