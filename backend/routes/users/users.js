221fe32204098558778aac21c3a9c25f297f7e11
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-01-30 03:31:41 -0500

    complete users get and post

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -2,30 +2,26 @@ const users = require("express").Router();
 const db = require('../../db/index.js');
 
 
-users.get("/", (req, res) => {
+users.get("/", async (req, res) => {
     try {
-        let usersDB = db.any("SELECT * FROM users");
+        let usersDB = await db.any("SELECT * FROM users");
         res.json({
             status: "success",
             message: "got all users",
-            body: {
-                usersDB
-            }
+            body: usersDB
         });
     } catch(error) {
         console.log(error);
     };
 });
 
-users.get("/:id", (req, res) => {
+users.get("/:id", async (req, res) => {
     try {
-        let usersDB = db.one ("SELECT * FROM users WHERE id = $1 ", [req.params.id]);
+        let usersDB =  await db.one("SELECT * FROM users WHERE id = $1 ", [req.params.id]);
         res.json({
             status: "success",
             message: "got specific user information",
-            body: {
-                usersDB
-            }
+            body: usersDB
         });
     } catch(error) {
         console.log(error);
@@ -39,8 +35,9 @@ users.post("/", (req, res) => {
             status: "success",
             message: "created new user",
             body: {
-                name: req.body.name,
-                age: req.body.age
+                name: req.body.first_name + req.body.last_name,
+                age: req.body.age,
+                about_statement: req.body.about_statement
             }
         });
     } catch(error) {
@@ -48,15 +45,15 @@ users.post("/", (req, res) => {
     };
 })
 
-users.delete("/:id", (req, res) => {
+users.delete("/:id", async (req, res) => {
     try {
-        db.none("DELETE FROM users WHERE id = $1", [req.params.id]);
+        db.none("DELETE FROM users WHERE id = " + req.params.id, [req.params.id]);
         res.json({
             status: "success",
             message: "deleted a single user",
             body: {
                 id: req.params.id,
-                name: req.body.name,
+                name: req.body.id,
                 age: req.body.age
             }
         });
