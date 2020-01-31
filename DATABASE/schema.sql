149673f74fca1055635c472bb3a59d111c5996f7
Kelvin <kelvinarellano@pursuit.org>
2020-01-30 19:10:59 -0500

    database tables

--- DATABASE/schema.sql
+++ DATABASE/schema.sql
@@ -1,15 +1,107 @@
-DROP DATABASE IF EXISTS trippin;
-CREATE DATABASE trippin;
+DROP DATABASE IF EXISTS Trippin_db;
+CREATE DATABASE Trippin_db;
+\c Trippin_db;
 
-\c trippin;
+CREATE TABLE users;
+(
+    user_id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    firstname TEXT,
+    lastname TEXT,
+    BIRTHDAY DATE,
+    PROFILE_PIC URL
+);
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS likes;
-DROP TABLE IF EXISTS post;
-DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS albums;
-DROP TABLE IF EXISTS picturs;
+CREATE TABLE post;
+(
+    post_id SERIAL PRIMARY KEY,
+    poster_id INT,
+    image url,
+    caption TEXT
+);
 
+CREATE TABLE comments;
+(
+    comment_id SERIAL PRIMARY KEY,
+    commenter_id INT,
+    post_id INT,
+    body TEXT
+);
 
+CREATE TABLE likes;
+(
+    like_id SERIAL PRIMARY KEY,
+    liker_id INT,
+    post_id INT
+);
+
+CREATE TABLE albums;
+(
+    album_id SERIAL PRIMARY KEY,
+    creator_id INT,
+    album_title TEXT
+);
+
+CREATE TABLE pictures;
+(
+    picture_id SERIAL PRIMARY KEY,
+    album_id INT,
+    photo_url text
+);
+
+INSERT INTO users
+    (username,password,firstname,lastname,birthday,profile_pic)
+VALUES
+(),
+(),
+(),
+(),
+();
+
+INSERT INTO post
+(poster_id,image,cation)
+VALUES
+(),
+(),
+(),
+(),
+();
+
+INSERT INTO comments
+(commentor_id,post_id,text)
+VALUES
+(),
+(),
+(),
+(),
+();
+
+INSERT INTO likes
+(liker-id,post_id)
+VALUES
+(1,1),
+(1,3),
+(3,1),
+(4,2),
+(5,1);
+
+INSERT INTO albums
+(creator_id, album_title)
+VALUES
+(1,'Movies'),
+(2, 'Memories'),
+(3, 'Family'),
+(4, 'Friends'),
+();
+
+INSERT INTO pictures
+(album_id, photo_url)
+VALUES
+(1,),
+(2,),
+(3,),
+(4,),
+(5,);
 
 
