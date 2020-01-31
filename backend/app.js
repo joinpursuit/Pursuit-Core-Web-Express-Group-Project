74fe04f68ed21624201f1d2ac79d0a72a90534fc
dannyl <danilovlaurent@pursuit.org>
2020-01-30 20:32:23 -0500

    correct file tree issue

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,11 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3000;
 
-
 const app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-app.listen(port,()=> {
-    console.log(`SERVER IS RUNNING ON PORT ${port}`)
-})
+app.listen(port, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${port}`);
+});
