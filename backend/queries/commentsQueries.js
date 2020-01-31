4de40a547a8bfd8f1adf2ccce7960bf56c2565a6
Phillip <phil.awich@gmail.com>
2020-01-30 19:53:03 -0500

    Add commentsQueires and comments routes

--- backend/queries/commentsQueries.js
+++ backend/queries/commentsQueries.js
@@ -1,1 +1,35 @@
 const db = require("../db/index.js")
+
+const getAllCommentsOnSinglePost = (res,req,next) => {
+    try{
+
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+const addSingleComment = (res,req,next) => {
+    try {
+
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+const editSingleComment = (res,req,next) => {
+    try {
+
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+const deleteSingleComment = (res,req,next) => {
+    try {
+
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+module.exports = {getAllCommentsOnSinglePost, addSingleComment, editSingleComment, deleteSingleComment}
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,3 +1,11 @@
 const comments = require("express").Router()
 
+comments.get("/posts/:post_id", getAllCommentsOnSinglePost)
+comments.post("/post/:post_id/:commenter_id", addSingleComment)
+comments.patch("/post_id/:commenter_id", editSingleComment)
+comments.delete("/:post_id/:commenter_id", deleteSingleComment)
+
+
+
+
 module.exports = comments
