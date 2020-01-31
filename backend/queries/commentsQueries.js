3cabbc3e82d5545c60eba51893aaca6b75809213
your name here <your_email_here>
2020-01-31 00:30:36 -0500

    creates likes routes

--- backend/queries/commentsQueries.js
+++ backend/queries/commentsQueries.js
@@ -1,1 +1,44 @@
-const db = require("../db/index.js")
+const db = require("../../db/index.js")
+
+const getAllCommentsByPost = async(req, res, next) => {
+    try {
+        res.status(200).json({
+            status: "Succcess"
+        })
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+const addCommentByPost = async(req, res, next) => {
+    try {
+        res.status(200).json({
+            status: "Success"
+        })
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+const editCommentByPost = async(req, res, next) => {
+    try {
+        res.status(200).json({
+            status: "Success"
+        })
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+const deleteComment = async(req, res, next) => {
+    try {
+        res.status(200).json({
+            status: "Success"
+        })
+    } catch(error) {
+        console.log("error")
+    }
+}
+
+
+module.exports = {getAllCommentsByPost, addCommentByPost, editCommentByPost, deleteComment};
