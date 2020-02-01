037b6ce7621e1728f263143ce8ffe056b4b088bc
Dugmar <dugmar.morocho@gmail.com>
2020-01-31 19:40:55 -0500

    albums routes creates

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3000;
+const albumsRouter = require("./routes/albums/albums");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use("/albums", albumsRouter);
 
 app.listen(port, () => {
   console.log(`SERVER IS RUNNING ON PORT ${port}`);
---
+++ backend/queries/albums/albums.js
@@ -0,0 +1,35 @@
+const dataBase = require("../../database/index.js");
+
+const getAlbums = async (req, res, next) => {
+  try {
+    let albums = await dataBase.any("SELECT * FROM albums WHERE id = $1", [
+      req.params.user_id
+    ]);
+    res.status(200).json({
+      albums,
+      status: "success",
+      message: "selected albums"
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const newAlbum = async (req, res, next) => {
+  try {
+    let newAlbum = await dataBase.any(
+      "INSERT INTO Albums (title, user_id) VALUES (${title},${user_id}) RETURNING *",
+      req.body
+    );
+
+    res.status(200).json({
+      newAlbum,
+      status: "success",
+      message: "one album created"
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAlbums, newAlbum };
