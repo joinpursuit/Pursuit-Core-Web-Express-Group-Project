a344ed8b8a2234f3aa1f8606974db3436e7782e5
crystal cardona <crystalcardona@pursuit.org>
2020-02-01 17:06:26 -0500

    pretty code for usersPost.js query

--- backend/queries/users/userPosts/userPosts.js
+++ backend/queries/users/userPosts/userPosts.js
@@ -2,7 +2,8 @@ const db = require("./../../../db/db")
 
 const getUserPost = async (req, res, next) => {
     try {
-        let post = await db.any("SELECT * FROM posts WHERE poster_id = $1", req.params.id);
+        let { postId } = req.params
+        let post = await db.any("SELECT * FROM posts WHERE poster_id = $1", postId);
         res.status(200).json({
             post,
             status: "Success",
