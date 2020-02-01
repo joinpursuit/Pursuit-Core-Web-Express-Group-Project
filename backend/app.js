9269dd7f7361cfcfaaf5dc50591dac26a61e7643
karenm09 <karenm929@hotmail.com>
2020-01-31 21:10:35 -0500

    add the postRouter to the server

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use("/posts", postsRouter)
+app.use("/posts", postsRouter);
 
 
 app.listen(port, ()=> {
