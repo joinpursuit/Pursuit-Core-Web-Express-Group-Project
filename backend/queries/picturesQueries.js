4c5bb924a3ca9e2bed03833410547f02ca872e63
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-02-01 15:40:44 -0500

    complteted picture query and route

--- backend/queries/picturesQueries.js
+++ backend/queries/picturesQueries.js
@@ -1,2 +1,57 @@
-const db = require("../db/index.js")
+const db = require("../../db/index")
 
+
+const getAllPictures = async (req, res, next) => {
+    try {
+      res.status(200).json({
+        status: "Success",
+        message: "Got all pictures",
+        body: {
+          pictures: await db.any("SELECT * FROM pictures WHERE album_id=1")
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+
+const addSinglePicture = async (req, res, next) => {
+    try {
+    let { album_id } = req.params;
+    let { post_id, pictureURL} = req.body;
+      let single_picture = await db.one("INSERT INTO pictures (post_id, pictureurl, album_id) VALUES ($1,$2,$3) RETURNING *", 
+        [post_id, pictureURL, album_id])
+      res.status(200).json({
+        status: "Success",
+        message: "You uploaded a single photo!",
+        body: { 
+            single_picture
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+const deleteSinglePicture = async (req, res, next) => {
+    try {
+      let { id } = req.params;
+      let user = await db.one("DELETE FROM pictures WHERE id=$1", id);
+      res.status(200).json({
+        status: "Success",
+        message: "You deleted a single photo!",
+        body: {
+          
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+
+module.exports = {getAllPictures, addSinglePicture, deleteSinglePicture};
