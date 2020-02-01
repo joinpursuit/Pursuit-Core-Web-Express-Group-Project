32cdd3be816e3762c9df9d88817c9a75a71796fd
crystal cardona <crystalcardona@pursuit.org>
2020-02-01 17:27:46 -0500

    made edits on getUserLogin

--- backend/queries/users/userLogins/userLogins.js
+++ backend/queries/users/userLogins/userLogins.js
@@ -3,7 +3,7 @@ const db = require("./../../../db/db");
 const getUserLogin = async (req, res, next) => {
     try {
     let {email, password} = req.params
-    let login = db.one("SELECT email, password FROM users WHERE id=$1, id=$2", [password, email]);
+    let login = db.one("SELECT email, password FROM users WHERE email=$1, password=$2", [email, password]);
     res.status(200).json({
         login,
         status: "Success",
