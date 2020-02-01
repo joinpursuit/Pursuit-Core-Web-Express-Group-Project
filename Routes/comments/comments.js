7eb2cd9300b3cddc70389d61388714a2b5f890aa
Marvin Thompson <marvinthompson@pursuit.org>
2020-02-01 16:19:11 -0500

    finishing up comments routes

--- Routes/comments/comments.js
+++ Routes/comments/comments.js
@@ -1,11 +1,20 @@
-const comments = require('express').Router()
-const { getAllComments, addSingleComment, editComment, deleteComment} = require('')
-//
+const comments = require('express').Router();
+const { getAllComments, addSingleComment, editComment, deleteComment} = require('');
 
-const commentRouter = require('../comments/comments.js')
-
+const commentRouter = require('../comments/comments.js');
+comments.use("/:id", commentRouter);
 // endpoints
 
+// get all comments
 comments.get("/posts/:post_id", getAllComments);
 
-comments.post("/posts/:post_id/:commenter_id")
+// add a single Comment
+comments.post("/posts/:post_id/:commenter_id", addSingleComment);
+
+//edit comment
+comments.patch("/posts/:post_id/:id", editComment);
+
+// delete a single comment
+comments.delete("/posts/:post_id/:id", deleteComment);
+
+
