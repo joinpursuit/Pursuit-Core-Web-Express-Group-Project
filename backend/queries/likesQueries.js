cd514a8abe8c9a9c45931f7ae669c65fb5285288
Kevin Wong <kevinwong@pursuit.org>
2020-01-31 22:27:16 -0500

    fix master branch, delete duplicate code

--- backend/queries/likesQueries.js
+++ backend/queries/likesQueries.js
@@ -1,83 +1,65 @@
-const db = require("../../db/index.js")
+const db = require("../../db/index.js");
 
-const getLikesForSingle = (req, res, next) => {
-    try {
-        res.status(200).json({
-            status: "Success",
-            message: "get All likes from post_id"
-        })
-    } catch(error) {
-        console.log("error")
-    }
-}
+const getLikesForSingle = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: "Success",
+      message: "get All likes from post_id",
+      body: {
+        searchPostID: req.params.post_id,
+        result: await db.any(
+          "SELECT * FROM posts JOIN LIKES ON posts.id = likes.post_id WHERE posts.id = $1",
+          req.params.post_id
+        )
+      }
+    });
+  } catch (error) {
+    res.json({
+      error: error
+    });
+  }
+};
 
-const addSingleLike = (req, res, next) => {
-    try{
-        res.status(200).json({
-            status: "Success",
-            message: "add a like to post_id by liker_id"
-        })
-    } catch(error) {
-        console.log("error")
-    }
-}
+const addSingleLike = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: "Success",
+      message: "add a like to post_id by liker_id",
+      body: {
+        liker_id: req.params.liker_id,
+        post_id: req.params.post_id,
+        result: await db.one(
+          "INSERT INTO likes (liker_id, post_id) VALUES($1, $2) RETURNING *",
+          [req.params.liker_id, req.params.post_id]
+        )
+      }
+    });
+  } catch (error) {
+    res.json({
+      error: error
+    });
+  }
+};
 
-const getLikesForSingle = async(req, res, next) => {
-    try {
-        res.status(200).json({
-            status: "Success",
-            message: "get All likes from post_id",
-            body: {
-                searchPostID:req.params.post_id,
-                result: await db.any('SELECT * FROM posts JOIN LIKES ON posts.id = likes.post_id WHERE posts.id = $1',req.params.post_id)
-              }
-        })
-    } catch(error) {
-        res.json({
-            error:error
-        })
-    }
-}
+const deleteSingleLike = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: "Success",
+      message: "delete a like by liker_id",
+      body: {
+        liker_id: req.params.liker_id,
+        post_id: req.params.post_id,
+        result: await db.one(
+          "DELETE FROM likes WHERE (liker_ID =$1 AND post_id=$2) RETURNING *",
+          [req.params.liker_id, req.params.post_id]
+        )
+      }
+    });
+  } catch (error) {
+    res.json({
+      error: error
+    });
+  }
+};
 
-const addSingleLike = async(req, res, next) => {
-    try{
-        res.status(200).json({
-            status: "Success",
-            message: "add a like to post_id by liker_id",
-            body:{
-                liker_id: req.params.liker_id,
-                post_id:req.params.post_id,
-                result: await db.one('INSERT INTO likes (liker_id, post_id) VALUES($1, $2) RETURNING *',[req.params.liker_id, req.params.post_id])
-            }
-        })
-    } catch(error) {
-        res.json({
-            error:error
-        })
-    }
-}
-
-const deleteSingleLike = async(req, res, next) => {
-    try{
-        res.status(200).json({
-            status: "Success",
-            message: "delete a like by liker_id"
-        })
-    } catch(error) {
-            message: "delete a like by liker_id",
-            body:{
-                liker_id: req.params.liker_id,
-                post_id:req.params.post_id,
-                result: await db.one('DELETE FROM likes WHERE (liker_ID =$1 AND post_id=$2) RETURNING *',[req.params.liker_id, req.params.post_id])
-            }
-        })
-    } catch(error) {
-        res.json({
-            error:error
-        })
-    }
-}
-
-
-
-module.exports = {getLikesForSingle, addSingleLike, deleteSingleLike}
+module.exports = { getLikesForSingle, addSingleLike, deleteSingleLike };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,6 @@ const posts = require("express").Router();
 const {
   getAllPosts,
   getSinglePost,
-  insertSinglePost
   insertSinglePost,
   updateSinglePost,
   deletePost
@@ -12,6 +11,6 @@ posts.get("/", getAllPosts); // Get all posts
 posts.get("/:id", getSinglePost); // Get single post
 posts.post("/", insertSinglePost); // Insert single post
 posts.patch("/:id", updateSinglePost);
-posts.delete("/:id", deletePost)
+posts.delete("/:id", deletePost);
 
 module.exports = posts;
