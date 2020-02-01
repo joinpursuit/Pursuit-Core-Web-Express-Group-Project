06014e13b0b697167d1f92a8e944ece43f3d0728
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-02-01 18:06:44 -0500

    working on comment query and routes

--- backend/queries/commentsQueries.js
+++ backend/queries/commentsQueries.js
@@ -1,1 +1,70 @@
 const db = require("../db/index.js")
+
+const getAllComments = async (req, res, next) => {
+    try{
+        res.status(200).json({
+            status: "Succes",
+            message: "Grabbed all comments",
+            body: {
+                comments: await db.any("SELECT * FROM comments WHERE post_id = $1 ")
+            }
+        });
+    } catch(error) {
+        next(error);
+    }
+};
+
+
+
+const addSingleComment = async (req, res, next) => {
+    try {
+        let { post_id, author_id } = req.params;
+        let { content } = req.body;
+            let single_comment = await db.one("INSERT INTO comments (post_id, author_id, content) VALUES ($1, $2, $3) RETURNING *", [post_id, author_id, content] )
+        res.status(200).json({
+            status: "Success",
+            message: "Added a single comment",
+            body: {
+                single_comment
+            }
+        });
+    } catch(error) {
+        next(error);
+    }
+}
+
+
+const editSingleComent = async (req, res, next) => {
+    try {
+        res.status(200).json({
+            status: "Succes",
+            message: "You have edited a single comment",
+            body: {
+
+            }
+        });
+    } catch(error) {
+        next(error);
+    }
+}
+
+
+
+const deleteSingleComment = async (req, res, next) => {
+    try {
+        res.status(200).json({
+            status: "Success",
+            message: "You have deleted a single comment",
+            body: {
+
+            }
+        });
+    }catch(error) {
+        next(error);
+    }
+}
+
+// GET /comments/posts/:post_id - Get all comments for a single post.
+// POST /comments/posts/:post_id/:commenter_id - Add single comment.
+// PATCH /comments/:post_id/:commenter_id - Edit single comment.
+// DELETE /comments/:post_id/:commenter_id - Delete single comment.
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,3 +1,12 @@
 const comments = require("express").Router()
+const {getAllComments, addSingleComment, editSingleComment, deleteSingleComment} = require("../queries/commentsQueries")
+
+comments.get("/posts/:post_id", getAllComments)  // get a single comment
+comments.post("/posts/:post_id/:author_id", addSingleComment)   //add a single comment
+comments.patch("/:post_id/:commenter_id", editSingleComment)   //edit a single comment
+comments.delete("/:post_id/:commenter_id", deleteSingleComment)   //delete a single comment
+
+// PATCH /comments/:post_id/:commenter_id - Edit single comment.
+// DELETE /comments/:post_id/:commenter_id - Delete single comment.
 
 module.exports = comments
