3758e59efe62a762c136610b005554fcfa5c04c2
Kevin Wong <kevinwong@pursuit.org>
2020-02-01 17:31:45 -0500

    add some input fields to signUp.html file, select them in signUp.js

--- frontEnd/signUp/signUp.js
+++ frontEnd/signUp/signUp.js
@@ -1,1 +1,5 @@
+let usernameInput = document.querySelector("#username");
+let passwordInput = document.querySelector("#password");
+let bioInput = document.querySelector("#bio");
+let propicURL = document.querySelector("#propicURL");
 
