145b95f74985e45588aa4e566fbd710ca32a7221
Dugmar <dugmar.morocho@gmail.com>
2020-01-31 22:29:44 -0500

    posts routes added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3000;
 const albumsRouter = require("./routes/albums/albums");
+const postsRouter = require("./routes/posts/posts");
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/albums", albumsRouter);
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`SERVER IS RUNNING ON PORT ${port}`);
