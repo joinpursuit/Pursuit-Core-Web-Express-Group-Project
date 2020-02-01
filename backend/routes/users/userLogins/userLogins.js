6c73b4024e2baf6a014acea6d069edd8383bdb73
crystal cardona <crystalcardona@pursuit.org>
2020-02-01 02:31:24 -0500

    added userlogin route

--- backend/routes/users/userLogins/userLogins.js
+++ backend/routes/users/userLogins/userLogins.js
@@ -1,3 +1,7 @@
 const userLogins = require("express").Router({mergeParams: true});
 
+let {getUserLogin} = require("./../../users/userLogins");
+
+userLogins.get("/logins/:email/:password", getUserLogin);
+
 module.exports = userLogins;
