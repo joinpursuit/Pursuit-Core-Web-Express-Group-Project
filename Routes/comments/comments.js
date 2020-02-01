9adf2b36df14d0bf7594aac66a19df1060d9b9eb
Marvin Thompson <marvinthompson@pursuit.org>
2020-02-01 15:49:05 -0500

    editing comments route

--- Routes/comments/comments.js
+++ Routes/comments/comments.js
@@ -1,6 +1,11 @@
 const comments = require('express').Router()
-const {}
+const { getAllComments, addSingleComment, editComment, deleteComment} = require('')
+//
 
 const commentRouter = require('../comments/comments.js')
 
 // endpoints
+
+comments.get("/posts/:post_id", getAllComments);
+
+comments.post("/posts/:post_id/:commenter_id")
