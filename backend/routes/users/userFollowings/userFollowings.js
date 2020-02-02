0e8b3227f6bd182b2a970431864280d2d559d92f
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 17:20:35 -0500

    add in routes for following/follower count

--- backend/routes/users/userFollowings/userFollowings.js
+++ backend/routes/users/userFollowings/userFollowings.js
@@ -1,7 +1,9 @@
 const userFollowings = require("express").Router({mergeParams: true});
-const {getUserFollowings, createUserFollowing} = require("./../../../queries/users/userFollowings/userFollowings");
+const {getUserFollowings, createUserFollowing, getUserFollowingCount, getUserFollowerCount} = require("./../../../queries/users/userFollowings/userFollowings");
 
 userFollowings.get("/", getUserFollowings);
 userFollowings.post("/", createUserFollowing);
+userFollowings.get("/followerCount", getUserFollowerCount);
+userFollowings.get("/followingCount", getUserFollowingCount);
 
 module.exports = userFollowings;
