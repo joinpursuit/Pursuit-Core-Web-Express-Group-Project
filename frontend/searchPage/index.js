999881cb16e58000eb828e793069a7a994a3dd64
david maravillas <davidmaravillas@pursuit.org>
2020-02-02 17:34:07 -0500

    add user form

--- frontend/searchPage/index.js
+++ frontend/searchPage/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("hello")
     let selectUsers = document.querySelector("#users")
+    let results = document.querySelector("#results")
 
     const fetchAllUsers = async(user)=>{
 
@@ -23,5 +24,35 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
     fetchAllUsers()
 
-    
+    let newUser = document.querySelector("#newUser");
+
+    newUser.addEventListener("submit", async (event)=>{
+        event.preventDefault()
+        results.innerHTML = ""
+
+        let userInfo = {first_name:newUser.firstName.value,last_name:newUser.lastName.value,age:newUser.age.value,about_statement:aboutStatement.value}
+        debugger
+        let userUrl = "http://localhost:3000/users/"
+
+        try{
+            let usersDb = await axios.post(userUrl,userInfo)
+            debugger
+            if(usersDb.status = 200){
+                let h4 = document.createElement("h4")
+                h4.innerText = `${usersDb.data.status} ${usersDb.data.message}`
+                results.appendChild(h4)
+            }
+            fetchAllUsers()
+
+            newUser.reset()
+        } catch (err){
+            console.log(err)
+        }
+
+
+    })
+
+    newUser.addEventListener("input",()=>{
+        results.innerHTML=""
+    })
 })
