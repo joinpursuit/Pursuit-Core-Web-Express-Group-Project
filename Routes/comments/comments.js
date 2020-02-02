2c4939a04217906ef7f134ad4faff197b7bcc811
Marvin Thompson <marvinthompson@pursuit.org>
2020-02-02 17:20:53 -0500

    removed doubles

--- Routes/comments/comments.js
+++ Routes/comments/comments.js
@@ -1,20 +1,12 @@
 const comments = require('express').Router();
 const { getAllComments, addSingleComment, editComment, deleteComment} = require('');
 
-const commentRouter = require('../comments/comments.js');
-comments.use("/:id", commentRouter);
-// endpoints
-
-// get all comments
 comments.get("/posts/:post_id", getAllComments);
 
-// add a single Comment
 comments.post("/posts/:post_id/:commenter_id", addSingleComment);
 
-//edit comment
 comments.patch("/posts/:post_id/:id", editComment);
 
-// delete a single comment
 comments.delete("/posts/:post_id/:id", deleteComment);
 
 module.exports = comments;
