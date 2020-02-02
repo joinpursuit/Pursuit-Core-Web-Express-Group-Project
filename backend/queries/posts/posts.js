cbb9f3d791eeb9db9e3408c170387a7822dcb2be
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 09:59:16 -0500

    add getPostById() function, and newDate() function

--- backend/queries/posts/posts.js
+++ backend/queries/posts/posts.js
@@ -1,5 +1,7 @@
 const db = require("./../../db/db");
 
+let newDate = () => new Date().toString()
+
 const getAllPosts = async (req, res) => {
     try {
         let posts = await db.any("SELECT * FROM posts INNER JOIN users ON posts.poster_id=users.id");
@@ -7,15 +9,38 @@ const getAllPosts = async (req, res) => {
             status: "success",
             message: "Retrieved all posts",
             posts,
-            timestamp: new Date().toString()  
+            timestamp: newDate()
         })
     } catch(error) {
         console.log(error)
         res.json({
             status: "error",
-            error
+            error,
+            timestamp: newDate()
         })
     }
-} // End of getAllPosts
+} // End of getAllPosts() function
 
-module.exports = {getAllPosts}
+const getPostById = async (req, res) => {
+    try {
+        let {postId} = req.params;
+        let post = await db.any("SELECT * FROM posts WHERE id=$1", postId);
+        if(post.length) {
+            res.json({
+                status: "success",
+                message: "Retrieved post at id " + postId,
+                post,
+                timestamp: newDate() 
+            })
+        }
+    } catch(error) {
+        console.log(error);
+        res.json({
+            status: "error",
+            error,
+            timestamp: newDate()
+        })
+    }
+} // End of getPostById() function
+
+module.exports = {getAllPosts, getPostById}
--- backend/routes/posts/posts.js
+++ backend/routes/posts/posts.js
@@ -1,11 +1,12 @@
 const posts = require("express").Router();
 const postCommentsRouter = require("./postComments/postComments");
 const postLikesRouter = require("./postLikes/postLikes");
-const {getAllPosts} = require("./../../queries/posts/posts");
+const {getAllPosts, getPostById} = require("./../../queries/posts/posts");
 
 posts.use("/:postId/comments", postCommentsRouter);
 posts.use("/:postId/likes", postLikesRouter);
 
 posts.get("/", getAllPosts);
+posts.get("/:postId", getPostById);
 
 module.exports = posts;
