c922c553798130f40dff55e602672ca59465e56e
david maravillas <davidmaravillas@pursuit.org>
2020-02-02 12:06:01 -0500

    add delete pictures

--- backend/routes/albums/pictures/pictures.js
+++ backend/routes/albums/pictures/pictures.js
@@ -21,7 +21,21 @@ pictures.post("/:album_id", async (req, res) => {
         let picturesDB = await db.any("INSERT INTO pictures (picture, album, poster_id) VALUES ($1, $2, $3) RETURNING *", [req.body.picture, Number(req.params.album_id),Number(req.body.poster_id)]);
         res.json({
             status: "success",
-            message: "created new album",
+            message: "posted new pictures",
+            body: picturesDB
+        });
+    } catch(error) {
+        console.log(error);
+    };
+});
+
+pictures.delete("/:pic_id", async (req, res) => {
+
+    try {
+        let picturesDB = await db.none("DELETE FROM pictures WHERE id = $1", [req.params.pic_id]);
+        res.json({
+            status: "success",
+            message: "Deleted picture",
             body: picturesDB
         });
     } catch(error) {
