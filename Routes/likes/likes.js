1e2657c2a324ac5efb0efbcc5ad7c6f2848a091a
Marvin Thompson <marvinthompson@pursuit.org>
2020-02-02 15:48:24 -0500

    likes queries finished

--- Routes/likes/likes.js
+++ Routes/likes/likes.js
@@ -1,19 +1,10 @@
 const likes = require('express').Router();
-const { getAllLikes, addSingleLike, deleteSingleLike} = require('');
-// these are my queries 
+const { getAllLikes, addSingleLike, deleteSingleLike} = require('../../queries/likes');
 
-const likesRouter = require('../likes/likes.js')
-likes.use('/:id', likesRouter);
-
-// api endpoints
-
-// get all likes for a single post
 likes.get("/:post_id", getAllLikes);
 
-// add a single like
 likes.post("/:post_id", addSingleLike);
 
-// delete a like
 likes.delete("/:id", deleteSingleLike);
 
 
