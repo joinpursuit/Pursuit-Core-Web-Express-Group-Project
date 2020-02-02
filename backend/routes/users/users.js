f7b55ce3c1a159131abf72111c12efd90a109ee7
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 15:13:23 -0500

    import middleware to users route

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -2,17 +2,17 @@ const users = require("express").Router();
 const userFollowingsRouter = require("./userFollowings/userFollowings");
 const userLoginsRouter = require("./userLogins/userLogins");
 const userPostsRouter = require("./userPosts/userPosts");
-const {} = require("./../../queries/users/users");
+const {getUsers, getUser, createUser, deleteUser} = require("./../../queries/users/users");
 
 users.use("/:userId/followings", userFollowingsRouter);
 users.use("/:userId/logins", userLoginsRouter);
 users.use("/:userId/posts", userPostsRouter);
 
-users.get("/");
-users.get("/:userId");
+users.get("/", getUsers);
+users.get("/:userId", getUser);
 users.get("/:userId/betHistory");
 users.patch("/:userId/betHistory");
-users.post("/");
-users.delete("/:userId");
+users.post("/", createUser);
+users.delete("/:userId", deleteUser);
 
 module.exports = users;
