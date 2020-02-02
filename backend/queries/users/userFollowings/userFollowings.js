670e4f8df807e55992e814c25f77397ddd67e071
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 14:58:18 -0500

    fix syntax in userFollowings queries

--- backend/queries/users/userFollowings/userFollowings.js
+++ backend/queries/users/userFollowings/userFollowings.js
@@ -2,11 +2,11 @@ const db = require("./../../../db/db")
 
 const getUserFollowings = async (req, res, next) => {
     try {
-    let followings = await db.any("SELECT * FROM followings WHERE followers_id =$1", req.params.id);
-    res.status(200).json({
-        followings,
-        status: "Success",
-        message: "All User Followings"
+        let followings = await db.any("SELECT * FROM followings WHERE followers_id =$1", req.params.id);
+        res.status(200).json({
+            followings,
+            status: "Success",
+            message: "All User Followings"
         })
     }catch(err){
         next(err)
@@ -15,11 +15,11 @@ const getUserFollowings = async (req, res, next) => {
 
 const createUserFollowing = async (req, res, next) => {
     try {
-    let following = await db.none("INSERT INTO followings VALUES (${followers_id}, ${followed_id}", req.params.id);
-    res.status(200).json({
-        following,
-        status: "Success",
-        message: "Created New Follower"
+        let following = await db.none("INSERT INTO followings VALUES (${followers_id}, ${followed_id}", req.params.id);
+        res.status(200).json({
+            following,
+            status: "Success",
+            message: "Created New Follower"
         })
     }catch(err){
         next(err)
