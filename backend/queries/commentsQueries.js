480cc1baa23ff99d7b191b4878494ffa0a5c7e2a
your name here <your_email_here>
2020-02-02 15:15:42 -0500

    fix folder name and album

--- backend/queries/commentsQueries.js
+++ backend/queries/commentsQueries.js
@@ -87,9 +87,3 @@ const deleteAllComments = async (req, res, next) => {
 
 module.exports = {getAllComments, addSingleComment, editSingleComment, deleteSingleComment, deleteAllComments};
 
-// GET /comments/posts/:post_id - Get all comments for a single post.
-// POST /comments/posts/:post_id/:commenter_id - Add single comment.
-// PATCH /comments/:post_id/:commenter_id - Edit single comment.
-// DELETE /comments/:post_id/:commenter_id - Delete single comment.
-
-// module.exports = {getAllCommentsByPost, addCommentByPost, editCommentByPost, deleteComment};
