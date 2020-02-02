787e613611cfef8f1bfb20070b1b627f59c434a8
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 16:55:57 -0500

    add getUserPosts query

--- backend/queries/users/userPosts/userPosts.js
+++ backend/queries/users/userPosts/userPosts.js
@@ -1,13 +1,22 @@
 const db = require("./../../../db/db")
+const {isUserExisting} = require("./../users");
 
 const getUserPost = async (req, res, next) => {
     try {
-        let post = await db.any("SELECT * FROM posts WHERE poster_id = $1", req.params.id);
-        res.status(200).json({
-            post,
-            status: "Success",
-            message: "All Users Posts"
-        })
+        if(await isUserExisting(req.params.userId)) {
+            let post = await db.any("SELECT * FROM posts WHERE poster_id = $1", req.params.userId);
+            res.status(200).json({
+                post,
+                status: "Success",
+                message: "All Users Posts"
+            })
+        } else {
+            res.json({
+                status: "error",
+                error: "No user found by that ID"
+            })
+        }
+        
     } catch(err){
         next(err)
     }
