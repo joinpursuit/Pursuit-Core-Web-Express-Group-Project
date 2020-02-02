311fa4cf7dfc3bd15115db803cdf7529eddaa036
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 10:23:21 -0500

    add deletePost() route and function

--- backend/queries/posts/posts.js
+++ backend/queries/posts/posts.js
@@ -2,22 +2,30 @@ const db = require("./../../db/db");
 
 let newDate = () => new Date().toString()
 
-const getAllPosts = async (req, res) => {
-    try {
-        let posts = await db.any("SELECT * FROM posts INNER JOIN users ON posts.poster_id=users.id");
-        res.status(200).json({
-            status: "success",
-            message: "Retrieved all posts",
-            posts,
-            timestamp: newDate()
-        })
-    } catch(error) {
-        console.log(error)
-        res.json({
+const sendError = (error) => {
+    console.log(error);
+    res.json({
             status: "error",
             error,
             timestamp: newDate()
         })
+} // End of sendError() function
+
+const successReq = (res, data, message) => {
+    res.status(200).json({
+        status: "success",
+        message: message,
+        data,
+        timestamp: newDate()
+    })
+} // End of successReq() function
+
+const getAllPosts = async (req, res) => {
+    try {
+        let posts = await db.any("SELECT * FROM posts INNER JOIN users ON posts.poster_id=users.id");
+        successReq(res, posts, "Retrieved all posts");
+    } catch(error) {
+        sendError(error);
     }
 } // End of getAllPosts() function
 
@@ -26,21 +34,23 @@ const getPostById = async (req, res) => {
         let {postId} = req.params;
         let post = await db.any("SELECT * FROM posts WHERE id=$1", postId);
         if(post.length) {
-            res.json({
-                status: "success",
-                message: "Retrieved post at id " + postId,
-                post,
-                timestamp: newDate() 
-            })
+            successReq(res, post, `Retrieved post at id ${postId}`);
         }
     } catch(error) {
-        console.log(error);
-        res.json({
-            status: "error",
-            error,
-            timestamp: newDate()
-        })
+        sendError(error);
     }
 } // End of getPostById() function
 
-module.exports = {getAllPosts, getPostById}
+const deletePost = async (req, res) => {
+    try {
+        let {postId} = req.params;
+        let post = await db.any("DELETE FROM posts WHERE id=$1 RETURNING *", postId);
+        if(post.length) {
+            successReq(res, post, `Deleted post at id ${postId}`);
+        }
+    } catch(error) {
+        sendError(error);
+    }
+} // End of deletePost() function
+
+module.exports = {getAllPosts, getPostById, deletePost}
--- backend/routes/posts/posts.js
+++ backend/routes/posts/posts.js
@@ -1,12 +1,13 @@
 const posts = require("express").Router();
 const postCommentsRouter = require("./postComments/postComments");
 const postLikesRouter = require("./postLikes/postLikes");
-const {getAllPosts, getPostById} = require("./../../queries/posts/posts");
+const {getAllPosts, getPostById, deletePost} = require("./../../queries/posts/posts");
 
 posts.use("/:postId/comments", postCommentsRouter);
 posts.use("/:postId/likes", postLikesRouter);
 
 posts.get("/", getAllPosts);
 posts.get("/:postId", getPostById);
+posts.delete("/:postId", deletePost);
 
 module.exports = posts;
