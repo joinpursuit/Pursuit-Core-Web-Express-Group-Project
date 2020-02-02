2cd0e6d6b02f09270ebebf7c214a03e299574b00
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 16:42:27 -0500

    add in error checks and fix login check in getUserLogin query

--- backend/queries/users/userLogins/userLogins.js
+++ backend/queries/users/userLogins/userLogins.js
@@ -1,13 +1,30 @@
 const db = require("./../../../db/db");
+const {isUserExisting} = require("./../users");
 
 const getUserLogin = async (req, res, next) => {
     try {
-    let login = db.one("SELECT email, password FROM users WHERE id=$1", req.params);
-    res.status(200).json({
-        login,
-        status: "Success",
-        message: "Users Login"
-        })
+        let {userId, email, password} = req.params
+        if(await isUserExisting(userId)) {
+            let login = db.any("SELECT * FROM users WHERE id=$1 AND email=$2 AND password=$3 RETURNING *", [userId, email, password]);
+            if(login.length) {
+                res.json({
+                    login,
+                    status: "Success",
+                    message: "logged in"
+                })
+            } else {
+                res.status(200).json({
+                    status: "error",
+                    error: "Email or password is incorrect"
+                })
+            }
+        } else {
+            res.json({
+                status: "error",
+                error: "No user existing by that ID"
+            })
+        }
+        
     } catch(err){
         next(err)
     }
