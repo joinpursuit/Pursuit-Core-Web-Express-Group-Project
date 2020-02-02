78600b4ff119379ba5e31f16fca3ca4831858091
karenm09 <karenm929@hotmail.com>
2020-02-02 11:43:26 -0500

    add semicolon to the end of the database name, and update posts table name in the insert table statement

--- queries/posts.js
+++ queries/posts.js
@@ -2,7 +2,7 @@ const db = require("../DATABASE/index.js");
 
 const getPosts = async (req, res, next) => {
    try {
-      let posts = await db.any("SELECT * FROM posts");
+      let posts = await db.any("SELECT username, image, caption FROM users JOIN posts on users.id = posts.poster_id");
       res.status(200).json({
          status: "Success",
          message: "Pull all posts",
