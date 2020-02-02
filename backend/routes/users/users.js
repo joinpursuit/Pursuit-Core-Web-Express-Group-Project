31c846fc27171e31b4e028009721415291905439
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-02 15:01:46 -0500

    add users routes

--- backend/routes/users/users.js
+++ backend/routes/users/users.js
@@ -2,9 +2,17 @@ const users = require("express").Router();
 const userFollowingsRouter = require("./userFollowings/userFollowings");
 const userLoginsRouter = require("./userLogins/userLogins");
 const userPostsRouter = require("./userPosts/userPosts");
+const {} = require("./../../queries/users/users");
 
 users.use("/:userId/followings", userFollowingsRouter);
 users.use("/:userId/logins", userLoginsRouter);
 users.use("/:userId/posts", userPostsRouter);
+
+users.get("/");
+users.get("/:userId");
+users.get("/:userId/betHistory");
+users.patch("/:userId/betHistory");
+users.post("/");
+users.delete("/:userId");
 
 module.exports = users;
