80545cfeeedd899ecd744d440cd6a530b809f962
John Jones Jr <jayjohnjones@gmail.com>
2020-02-02 17:38:01 -0500

    preferences queries fixed

--- Routes/preferences/preferences.js
+++ Routes/preferences/preferences.js
@@ -1,12 +1,12 @@
 const preferences = require("express").Router();
-const { getAllPreferences, createPreference, editPreference } = require("");
+const { getAllPreferences, createPreference, editPreference } = require("../../queries/preferences");
 
 
 preferences.get("/:user_id", getAllPreferences);
 
 preferences.post("/:user_id", createPreference);
 
-preferences.patch("/:user_id/:id", editPreference);
+preferences.put("/:user_id/", editPreference);
 
 
 module.exports = preferences;
--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser")
 
-const userRouter = require('./routes/users/users.js')
-const likesRouter = require('./routes/likes/likes.js')
-const postRouter = require('./routes/posts/post.js')
-const commentRouter = require('./routes/comments/comment.js')
-const albumsRouter = require('./routes/albums/albums.js')
-const picturesRouter = require('./routes/pictures/pictures.js')
+// const userRouter = require('./routes/users/users.js')
+// const likesRouter = require('./routes/likes/likes.js')
+// const postRouter = require('./routes/posts/post.js')
+// const commentRouter = require('./routes/comments/comment.js')
+// const albumsRouter = require('./routes/albums/albums.js')
+// const picturesRouter = require('./routes/pictures/pictures.js')
 const preferencesRouter = require('./routes/preferences/preferences.js')
 
 const port = 3000;
@@ -19,12 +19,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.use("/users", userRouter)
-app.use("/likes", likesRouter)
-app.use("/posts", postRouter)
-app.use("/comments", commentRouter)
-app.use("/albums", albumsRouter)
-app.use("/pictures", picturesRouter)
+// app.use("/users", userRouter)
+// app.use("/likes", likesRouter)
+// app.use("/posts", postRouter)
+// app.use("/comments", commentRouter)
+// app.use("/albums", albumsRouter)
+// app.use("/pictures", picturesRouter)
 app.use("/preferences", preferencesRouter)
 
 
