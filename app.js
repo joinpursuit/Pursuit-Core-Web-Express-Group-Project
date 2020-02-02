c19a826e13ae526d647d036234f2457d173ead0d
rafidhossain1995 <rafidhossain@pursuit.org>
2020-02-02 12:14:31 -0500

    edit album and like routes, however like route can't get likes back

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const port = 3000
 const app = express()
 const postsRouter = require("./routes/posts/posts.js")
 const picturesRouter = require("./routes/pictures/pictures.js")
+const albumRouter = require("./routes/albums/albums")
+const likesRouter = require("./routes/likes/likes")
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use("/posts", postsRouter)
 app.use("/pictures", picturesRouter)
+app.use("/albums",albumRouter)
+app.use("/likes",likesRouter)
 
 app.listen(port, () => {
   console.log("The server is currently running on port " + port)
