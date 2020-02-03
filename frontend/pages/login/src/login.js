cec809032cec12b06086e0a251a2c9939e4d8b33
Wilsantos1975 <faustosantos@pursuit.org>
2020-02-03 14:30:42 -0500

    create all buttons and input

---
+++ frontend/pages/login/src/login.js
@@ -0,0 +1,8 @@
+let login = document.querySelector("logIn");
+let signUp = document.querySelector("signUp");
+let logInForm = document.querySelector("logInForm");
+let formSignIn = document.querySelector("signInForm");
+let logo = document.querySelector("logo");
+
+
+
