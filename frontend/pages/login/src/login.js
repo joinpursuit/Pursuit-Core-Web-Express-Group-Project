71bb6ef304901b45942590647a873631dc3f1ad5
Wilsantos1975 <faustosantos@pursuit.org>
2020-02-03 15:32:48 -0500

    styling buttons and fixed eventlisterner error

--- frontend/pages/login/src/login.js
+++ frontend/pages/login/src/login.js
@@ -1,8 +1,9 @@
-let login = document.querySelector("logInButton");
-let signUp = document.querySelector("signUp");
-let logInForm = document.querySelector("logInForm");
-let formSignIn = document.querySelector("signInForm");
-let logo = document.querySelector("logo");
+let login = document.querySelector("#logInButton");
+let signUp = document.querySelector("#signUp");
+let logInForm = document.querySelector("#logInForm");
+let formSignIn = document.querySelector("#signInForm");
+let logo = document.querySelector("#logo");
+let h1 =document.querySelector("h1")
 
 login.addEventListener("click" ,() => {
     h1.innertext = "Welcome Back";
