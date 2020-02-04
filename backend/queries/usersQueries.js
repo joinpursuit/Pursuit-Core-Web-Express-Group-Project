76824513c79862d9cdc29cd0d68d6bf58b6df4eb
your name here <your_email_here>
2020-02-03 21:56:41 -0500

    add deleted comment by owner

--- backend/queries/usersQueries.js
+++ backend/queries/usersQueries.js
@@ -83,8 +83,9 @@ const deleteUsersById = async (req, res, next) => {
 const searchUserByName = async (req, res, next) => {
   try {
     let { username } = req.params;
+    // console.log(username)
     let user = await db.one(
-      "SELECT * FROM users WHERE username = $1 RETURNING *",
+      "SELECT * FROM users WHERE username = $1",
       username
     );
     if (user) {
