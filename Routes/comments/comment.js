1b2b89f42b49d03f07ca0b9bec12f44e7f675029
John Jones Jr <jayjohnjones@gmail.com>
2020-02-03 19:34:02 -0500

    Fixed Comment route

--- Routes/comments/comment.js
+++ Routes/comments/comment.js
@@ -1,12 +1,12 @@
 const comments = require('express').Router();
-const { getAllComments, addSingleComment, editComment, deleteComment} = require('./../queries/comments');
+const { getAllComments, addSingleComment, editSingleComment, deleteComment} = require('../../queries/comments');
 
 
 comments.get("/posts/:post_id", getAllComments);
 
 comments.post("/posts/:post_id/:commenter_id", addSingleComment);
 
-comments.patch("/posts/:post_id/:id", editComment);
+comments.patch("/posts/:post_id/:id", editSingleComment);
 
 comments.delete("/posts/:post_id/:id", deleteComment);
 
