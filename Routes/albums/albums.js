547cf11f42e77cabb1324d3b5bd0654d1bc4fae6
Marvin Thompson <marvinthompson@pursuit.org>
2020-02-03 21:04:51 -0500

    final backend edits

--- Routes/albums/albums.js
+++ Routes/albums/albums.js
@@ -1,8 +1,6 @@
 const albums = require('express').Router()
 const {getAlbums,getAlbum,createAlbum,deleteAlbum} = require('../../queries/albums') 
 // (queries go above in require )
-// const albumPictureRouter = require('./pictures/pictures.js')
-// albums.use("/:id/pictures",albumPictureRouter)
 
 albums.get('/',getAlbums)
 
--- Routes/albums/pictures/pictures.js
+++
@@ -1,7 +0,0 @@
-const albumsPictures = require('express').Router({mergeParams: true})
-const {getPicturesFromAlbums} = require('')
-// (queries go above in require )
-
-albumsPictures.get('/',getPicturesFromAlbums)
-
-module.exports = albumsPictures
