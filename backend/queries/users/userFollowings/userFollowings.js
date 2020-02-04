4b98430ec8c3022d54995abe5fa5360858c1e6b6
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-04 18:40:13 -0500

    replace 200 status json with a throw error in getUserFollwoerCount query

--- backend/queries/users/userFollowings/userFollowings.js
+++ backend/queries/users/userFollowings/userFollowings.js
@@ -63,10 +63,7 @@ const getUserFollowerCount = async (req, res, next) => {
                 })
             }
         } else {
-            res.json({
-                status: "error",
-                error: "No user found by that ID"
-            })
+            throw {status: 404, error: "User does not exist"}
         }
 
     } catch(err) {
