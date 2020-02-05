c9eeaf0c80452bdf01111064dad993bf604358c0
bsihame <sihamebazi@pursuit.org>
2020-02-04 22:05:24 -0500

    update login.js functionality

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,13 @@ const bodyParser = require("body-parser");
 const port = 3000;
 const app = express();
 const postsRouter = require("../routes/posts/posts.js")
+const usersRouter = require("../routes/users/users")
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("/posts", postsRouter);
+app.use("/users", usersRouter);
 
 
 app.listen(port, ()=> {
