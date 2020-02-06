70d60bd77682d372dc46c172b2e2e675ab652d11
bsihame <sihamebazi@pursuit.org>
2020-02-05 22:12:55 -0500

    work in if statment login js

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -10,12 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
       try {
         let res = await axios.get(`http://localhost:3000/users`)
-         debugger
+         
         res.data.users.forEach(user => {
-          debugger
-          if(userName === user.username && inputPassword === user.password) {
-           window.location.href = "frontend/activeUser/activeUser.html"
+          
+          if(userName === user.username || inputPassword === user.password) {
+           window.location.pathname = "Users/bsihame/Desktop/groupProject/Pursuit-Core-Web-Express-Group-Project/frontend/activeUser/activeUser.html"
+          } else {
+            debugger
           }
+        
         })
       } catch (err) {
         console.log(err)
