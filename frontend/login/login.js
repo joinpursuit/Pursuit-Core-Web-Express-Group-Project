b796f60453c44c012b26751d3501d3a3d08d4967
bsihame <sihamebazi@pursuit.org>
2020-02-05 19:51:10 -0500

    fix signUp html

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,7 +1,25 @@
-document.addEventListener("DOMContentLoaded", () =>{
-  let loginButton = document.querySelector("#logInBtn");
-  let loginForm = document.querySelector("#loginForm");
-  loginButton.addEventListener("click", ()=> {
-    
-  })
+document.addEventListener("DOMContentLoaded", () => {
+  let loginForm = document.querySelector(".loginForm");
+  let userName = document.querySelector("#userLoginInput").value;
+  
+  
+  let password = document.querySelector("#loginPasswordInput");
+  let inputPassword = password.value;
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+      try {
+        let res = await axios.get(`http://localhost:3000/users`)
+         debugger
+        res.data.users.forEach(user => {
+          debugger
+          if(userName === user.username && inputPassword === user.password) {
+           window.location.href = "frontend/activeUser/activeUser.html"
+          }
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  )
 })
