b12e535f06c72e718bb1681865dce1078df8ba81
BrandonBrown <brandonbrown@pursuit.org>
2020-02-07 15:10:06 -0500

    correctFollowingCount

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ let userId = 1
 const getFollowing = async () => {
     try {
         let res = await axios.get(`http://localhost:3000/users/${userId}/followings/followingCount`) 
-        let following = res.data.following[0].userfollowingcount
-        console.log(following.userfollowingcoun);
+        let following = res.data.following[0]
+        console.log(following.userfollowingcount);
          
     } catch(error) {
         console.log(error)
@@ -17,7 +17,7 @@ const getFollowers = async () => {
     try {
         let res = await axios.get(`http://localhost:3000/users/${userId}/followings/followingCount`) 
         let following = res.data.following[0].userfollowingcount
-        console.log(following.userfollowingcoun);
+        console.log(following.userfollowingcount);
          
     } catch(error) {
         console.log(error)
