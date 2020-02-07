4cc0566a736bae1da76de2c4df23a323ab9f07bf
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-07 11:35:52 -0500

    split bet-history in users table into two separate columns of bit_wins and bet_losses

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -19,7 +19,8 @@ CREATE TABLE users
     state TEXT,
     username VARCHAR, 
     password VARCHAR,
-    bet_history VARCHAR
+    bet_wins INT,
+    bet_losses INT
 );
 
 
@@ -68,11 +69,11 @@ CREATE TABLE bets (
 );
 
 
-INSERT INTO users (full_name, birth_date, city, state, username, password, bet_history)
-VALUES ('Isaiah Collazo', '1999-10-01', 'Brooklyn', 'NY', 'theycallme_zay', 'expo', '4-0'),
-('Brandon Floyd-Mayweather Brown', '1996-02-06', 'Brooklyn', 'NY', 'brandonbrown@pursuit.org', 'secret', '50-0'),
-('Crystal Cardona', '1992-02-09', 'New York', 'NY', 'crystalcardona@pursuit.org', 'yerrrr', '5-0' ),
-('Wil Santos', '1986-04-14', 'Brooklyn', 'NY', 'wilghensantos@gmail.com', 'deLoMio','12-1');
+INSERT INTO users (full_name, birth_date, city, state, username, password, bet_wins, bet_losses)
+VALUES ('Isaiah Collazo', '1999-10-01', 'Brooklyn', 'NY', 'theycallme_zay', 'expo', 4, 0),
+('Brandon Floyd-Mayweather Brown', '1996-02-06', 'Brooklyn', 'NY', 'brandonbrown@pursuit.org', 'secret', 50, 0),
+('Crystal Cardona', '1992-02-09', 'New York', 'NY', 'crystalcardona@pursuit.org', 'yerrrr', 5, 0 ),
+('Wil Santos', '1986-04-14', 'Brooklyn', 'NY', 'wilghensantos@gmail.com', 'deLoMio',12, 1);
 
 INSERT INTO posts (poster_id, body)
 VALUES (1, 'Whats crackin'),
