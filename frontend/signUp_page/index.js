de0f11e12fc6db618c55242751057d388b30970d
Marvin Thompson <marvinthompson@pursuit.org>
2020-02-06 20:31:43 -0500

    finished page

--- frontend/signUp_page/index.js
+++ frontend/signUp_page/index.js
@@ -16,28 +16,34 @@ document.addEventListener("DOMContentLoaded", () => {
     let orientation = document.querySelector("#orientation")
 
     // preferences
-    let haveKidsYes = document.querySelector("#yes1")
-    let haveKidsNo = document.querySelector("#no1")
-    let wantKidsYes = document.querySelector("#yes2")
-    let wantKidsNo = document.querySelector("#no2")
-    let drinkYes = document.querySelector("#yes3")
-    let drinkNo = document.querySelector("#no3")
-    let smokeYes = document.querySelector("#yes4")
-    let smokeNo = document.querySelector("#no4")
-    let longTermYes = document.querySelector("#yes5")
-    let longTermNo = document.querySelector("#no5")
+    let haveKidsYes = document.getElementById("yes1")
+    let wantKidsYes = document.getElementById("yes2")
+    let drinkYes = document.getElementById("yes3")
+    let smokeYes = document.getElementById("yes4")
+    let longTermYes = document.getElementById("yes5")
+ 
     //
 
-    const getRadioValue = () => {
+    // const checkButtonHaveKids = () => {
+    //     let whichButton = document.getElementsByName("haveKids")
+    //     let length = whichButton.length 
         
-    }
+    //     for (let i = 0; i < length, i++) {
+    //         if (whichButton[i].checked) {
+
+    //         }
+    //     }
+    // }
 
     form.addEventListener("submit", async (e) => {
         e.preventDefault()
+        debugger
         try {
             let res = await axios.post("http://localhost:3000/users", {firstName: firstName.value, lastName: lastName.value, userName: userName.value, password: passWord.value, email: email.value, dob: dob.value, gender: gender.options[gender.options["selectedIndex"]].value, orientation: orientation.options[orientation.options["selectedIndex"]].value})
             debugger
-            let res2 = await axios.post(`http://localhost:3000/preferences/${res.data.user_id.id}`, )
+            let res2 = await axios.post(`http://localhost:3000/preferences/${res.data.user_id.id}`, {users_id:res.data.user_id.id, do_have_child: haveKidsYes.checked, want_child: wantKidsYes.checked, drink: drinkYes.checked, smoke_weed: smokeYes.checked, long_term: longTermYes.checked})
+
+            debugger
             
         } catch (error) {
             console.log(error)
