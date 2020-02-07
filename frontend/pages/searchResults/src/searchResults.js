2b4230521719ed398aebac52d4640bc5e0f84bbc
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-07 13:40:02 -0500

    display no users found on a bad search

--- frontend/pages/searchResults/src/searchResults.js
+++ frontend/pages/searchResults/src/searchResults.js
@@ -1,19 +1,26 @@
 let userQuery = "v";
 let search = document.querySelector("#search");
 let searchForm = document.querySelector("#searchForm");
+let searchResponse = document.querySelector("#searchResponse");
 
 const fetchData = async (url, cb) => {
     try {
         let res = await axios.get(url);
         cb(res.data);
     } catch (err) {
-        if(err.response) cb(err.response);
+        if(err.response) cb(err.response.data);
         console.log(err);
     }
 } // End of fetchData() function
 
 const findUser = (data) => {
-    debugger;
+    searchResponse.innerHTML = "";
+    if(data.error) {
+        let error = document.createElement("error");
+        error.innerText = data.error;
+        error.id = "error";
+        searchResponse.appendChild(error);
+    }
 } // End of findUser() function
 
 fetchData(`http://localhost:3000/users?search=${userQuery}`, findUser);
