34eb33198719873fb2d50a0dbcd4b037dc9cc57c
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-07 13:58:04 -0500

    add ability to search on results page

--- frontend/pages/searchResults/src/searchResults.js
+++ frontend/pages/searchResults/src/searchResults.js
@@ -13,25 +13,36 @@ const fetchData = async (url, cb) => {
     }
 } // End of fetchData() function
 
+const findUserId = () => {
+    debugger;
+} // End of findUserId() function
+
 const findUser = (data) => {
     searchResponse.innerHTML = "";
     if(data.error) {
-        let error = document.createElement("error");
+        let error = document.createElement("p");
         error.innerText = data.error;
         error.id = "error";
         searchResponse.appendChild(error);
     } else {
         let userList = document.createElement("ul");
         let users = data.users;
-        users.forEach(user => {
+        for(let i = 0; i < users.length; i++) {
+            if(i === 10) break;
+            let user = users[i];
             let li = document.createElement("li");
             li.innerHTML = `<a class="users" id=${user.id} href="./../userProfile/userProfile.html">${user.full_name}</a>`
             userList.appendChild(li);
-        })
+        }
         searchResponse.appendChild(userList);
+        userList.addEventListener("click", findUserId);
     }
 } // End of findUser() function
 
-
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    userQuery = search.value;
+    fetchData(`http://localhost:3000/users?search=${userQuery}`, findUser);
+}) // End of searchForm event listener
 
 fetchData(`http://localhost:3000/users?search=${userQuery}`, findUser);
