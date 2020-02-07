9f236907298053db4f457734a4ce6b8ae9ac4de6
Kelvin <kelvinarellano@pursuit.org>
2020-02-06 19:27:53 -0500

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-//const axios = require("axios")
 const bodyParser = require("body-parser");
 const port = 3000;
 const app = express();
-const postsRouter = require("../routes/posts/posts.js")
-const usersRouter = require("../routes/users/users.js")
-
+const postsRouter = require("../routes/posts/posts.js");
+const commentsRouter = require("../routes/comments/comments");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("/posts", postsRouter);
-app.use("/users", usersRouter)
+app.use("/comments", commentsRouter);
+
 
 
 app.listen(port, ()=> {
