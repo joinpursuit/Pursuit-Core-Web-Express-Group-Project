6d68c531d3a6f515765e97b498413705fe363a2d
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-02-07 20:57:26 -0500

    create createUpcomingElement() function

--- frontend/pages/bets/src/bets.js
+++ frontend/pages/bets/src/bets.js
@@ -1,4 +1,8 @@
 let upcomingGames = document.querySelector(".upcomingGames");
+let gameSelect = document.querySelector("#betGame");
+let teamSelect = document.querySelector("#betTeam");
+let betForm = document.querySelector("#betForm");
+let games = {};
 
 const fetchData = async (url, cb) => {
     try {
@@ -10,7 +14,16 @@ const fetchData = async (url, cb) => {
 } // End of fetchData() function
 
 const createUpcomingElement = (event) => {
-
+    let option = document.createElement("option");
+    let section = document.createElement("section");
+    option.innerText = `${event.teams[0].name} VS. ${event.teams[1].name}`;
+    option.value = event.event_id;
+    let h3 = document.createElement("h3");
+    h3.innerText = `${event.teams[0].name} VS. ${event.teams[1].name}`;
+    section.appendChild(h3);
+    upcomingGames.appendChild(section);
+    games[event.event_id] = event.teams;
+    gameSelect.appendChild(option);
 } // End of createUpcomingElement() function
 
 const getUpcomingGames = (data) => {
@@ -22,3 +35,15 @@ const getUpcomingGames = (data) => {
 } // End of getUpcomingGames() function
 
 fetchData("http://localhost:3000/sports/4/events", getUpcomingGames);
+
+gameSelect.addEventListener("change", (e) => {
+    teamSelect.innerHTML = "<option value='disabled' selected disabled>Select a Team</option>"
+    let teams = games[e.target.value];
+    teams.forEach(team => {
+        let option = document.createElement("option");
+        option.innerText = team.name;
+        option.value = team.team_id;
+        teamSelect.appendChild(option);
+    })
+})
+
